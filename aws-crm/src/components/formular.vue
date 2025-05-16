39745207df4c2addef3084eca3e20bd0f51958d7
LeGia179 <Le_g_minh@outlook.de>
2025-05-16 22:35:00 +0200

    C&P Test02 build pipeline

--- aws-crm/src/components/formular.vue
+++ aws-crm/src/components/formular.vue
@@ -47,7 +47,7 @@ const handlePurchase = () => {
   <div class="ui-container">
     <!-- Linker Bereich -->
     <div class="leftside">
-      <h3>Kundendaten das ist ein Test Header für die Pipeline</h3>
+      <h3>Kundendaten das Unfassbar</h3>
       <label>Nachname</label>
       <input v-model="nachName" type="text" placeholder="Nachname" required />
 
